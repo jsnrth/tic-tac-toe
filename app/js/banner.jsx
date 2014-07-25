1d151e2155578efa45a32a72aecb2b2a99a7927f
Jason <jasonmichaelroth@gmail.com>
2014-07-25 11:24:10 -0600

    add game banner

---
+++ app/js/banner.jsx
@@ -0,0 +1,22 @@
+var React = require('react');
+
+module.exports = React.createClass({
+    statusText: function(){
+        if(this.props.status.done){
+            if(this.props.status.winner)
+                return "Winner! " + this.props.status.winner.toUpperCase();
+            else
+                return "Tie!"
+        }
+        else
+            return "Next: " + this.props.status.player.toUpperCase();
+    },
+
+    render: function(){
+        return (
+            <div>
+                {this.statusText()}
+            </div>
+        );
+    }
+});
--- app/js/game.jsx
+++ app/js/game.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var Board = require('./board');
+var Banner = require('./banner');
 var Game = require('../../lib/game');
 
 module.exports = React.createClass({
@@ -11,13 +12,29 @@ module.exports = React.createClass({
         return this.state.board;
     },
 
+    getStatus: function(){
+        return {
+            winner: this.state.winner,
+            player: this.state.player,
+            done: !!(this.state.winner || this.state.moves == 0)
+        };
+    },
+
     handleMove: function(position){
-        this.replaceState(Game.move(this.state, position));
+        try {
+            this.replaceState(Game.move(this.state, position));
+        }
+        catch(e) {
+            console.warn("Ignoring move: " + e.message);
+        }
     },
 
     render: function(){
         return (
-            <Board board={this.getBoard()} handleMove={this.handleMove}/>
+            <div>
+                <Board board={this.getBoard()} handleMove={this.handleMove}/>
+                <Banner status={this.getStatus()}/>
+            </div>
         );
     }
 });
