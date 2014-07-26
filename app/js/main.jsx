96951c6f6506c7f9b56a74429fba6cf199a3ccb9
Jason <jasonmichaelroth@gmail.com>
2014-07-26 08:03:47 -0600

    use strict everywhere

--- app/js/main.jsx
+++ app/js/main.jsx
@@ -1,3 +1,5 @@
+"use strict";
+
 var React = require('react');
 var Game = require('./game');
 React.renderComponent(Game(), document.getElementById('game'));
--- test/board-test.js
+++ test/board-test.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var assert = require('assert');
 var Board = require('../lib/board');
 
